4b29d1c7657df58f3972ee7487565a859733ccc0
Chad Miller <cmiller86@gatech.edu>
2015-09-23 10:35:17 -0400

    file thing uses file as a string

--- fileio.py
+++ fileio.py
@@ -2,11 +2,11 @@
 
 import cmath, re, sys
 
-def get_header_data( data_file ):
+def get_header_data( data_string ):
 	retval    = dict()
 	end_count = 0
 	
-	for line in data_file:
+	for line in data_string.split( '\n' ):
 		line = line.lstrip().rstrip()
 		
 		if( line[0] != '$' ):
@@ -18,32 +18,53 @@ def get_header_data( data_file ):
 			
 			if( end_count > 1 ):
 				return retval
-			
 
 '''
 X/Y data:
 X-axis = time
 Y-axis = complex number
 '''
-def get_xy_data( data_file ):
-	retval = list()
+def get_xy_data( data_string ):
+	retval          = list()
+	data_string_arr = data_string.split( '\n' )
+	count           = 0
+	end_count       = 0
 	
-	for line in data_file:
+	for line in data_string_arr:
+		if( line.lstrip().strip() == '$END' ):
+			end_count += 1
+		
+		count += 1
+		
+		if( end_count > 1 ):
+			break
+	
+	for line in data_string_arr[count:]:
 		line = line.lstrip().rstrip()
 		match_obj = re.match( r"(.+)\s+(.+)", line )
-		retval.append( complex( float( match_obj.group(1) ), float( match_obj.group(2) ) ) )
+		
+		if match_obj is not None:
+			retval.append( complex( float( match_obj.group(1) ), float( match_obj.group(2) ) ) )
 		
 	return retval
 
 def main( argv ):
 	data_file   = open( argv[0], 'r' )
-	header_data = get_header_data( data_file )
-	xy_data     = get_xy_data( data_file )
+	data_string = ''
+	
+	for line in data_file:
+		data_string += line
+	
+	header_data = get_header_data( data_string )
+	xy_data     = get_xy_data( data_string )
+	
+	print( 'header_data is:' )
 	
 	for key, val in header_data.items():
 		print( '{} = {}'.format( key, val ) )
 	
 	counter = 1
+	print( 'xy_data is:' )
 	
 	for item in xy_data:
 		print( '{} : {}'.format( counter, item ) )
